1360643e7efa3a7ac08017fed3ffa0ecf54bd8b9
lpinto-eu <mail@lpinto.eu>
2013-10-30 23:33:01 +0000

    Added titles on the links.

--- js/app/router.js
+++ js/app/router.js
@@ -2,6 +2,6 @@ App.Router.map(function () {
     this.resource('animals', function () {
         this.route('new')
     }),
-            
-    this.resource("animal", { path: ":animal_id"})
+
+    this.resource("animal", { path: "animals/:animal_id"})
 });
