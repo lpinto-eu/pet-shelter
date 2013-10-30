ba6f1790d6b11f45152b66e6b7bd6fbbecd2c583
lpinto-eu <mail@lpinto.eu>
2013-10-30 00:32:45 +0000

    Animals' description Items

--- js/app/views/animalSex.js
+++ js/app/views/animalSex.js
@@ -6,7 +6,7 @@ App.AnimalSexView = Ember.View.extend({
         if (this.content === "F") {
             return "fa fa-female text-danger";
         } else if (this.content === "M") {
-            return "fa fa-male text-success";
+            return "fa fa-male text-primary";
         }
     }.property('content')
 });
