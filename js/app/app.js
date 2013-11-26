887cdff8806ac609f9209bd1f4f7f8d645e62d50
lpinto-eu <mail@lpinto.eu>
2013-11-26 23:10:09 +0000

    Image upload and preview

--- js/app/app.js
+++ js/app/app.js
@@ -6,3 +6,29 @@ window.App = Ember.Application.create();
 Ember.Handlebars.registerBoundHelper('date', function(date) {
   return moment(date).fromNow();
 });
+
+/*
+ * Foto upload and Preview
+ */
+App.FotoPreview= Ember.View.extend({
+    attributeBindings: ['src'],
+    tagName: 'img',
+});
+
+App.FotoUp= Ember.TextField.extend({
+    type: 'file',
+
+    change: function(evt) {
+        var input = evt.target;
+        if (input.files && input.files[0]) {
+            var that = this;
+
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var data = e.target.result;
+                that.set('parentView.content', data);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    },
+});
