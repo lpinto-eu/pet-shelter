bc6d8098c33213f38d1f1b3d7d2139da79277c04
lpinto-eu <mail@lpinto.eu>
2013-11-27 00:42:54 +0000

    Select element for sex

--- js/app/app.js
+++ js/app/app.js
@@ -3,32 +3,47 @@ window.App = Ember.Application.create();
 /*
  * Helpers for handlebars
  */
-Ember.Handlebars.registerBoundHelper('date', function(date) {
-  return moment(date).fromNow();
+Ember.Handlebars.registerBoundHelper('date', function (date) {
+    return moment(date).fromNow();
 });
 
 /*
  * Foto upload and Preview
  */
-App.FotoPreview= Ember.View.extend({
+App.FotoPreview = Ember.View.extend({
     attributeBindings: ['src'],
-    tagName: 'img',
+    tagName: 'img'
 });
 
-App.FotoUp= Ember.TextField.extend({
+App.FotoUp = Ember.TextField.extend({
     type: 'file',
 
-    change: function(evt) {
+    change: function (evt) {
         var input = evt.target;
         if (input.files && input.files[0]) {
             var that = this;
 
             var reader = new FileReader();
-            reader.onload = function(e) {
+            reader.onload = function (e) {
                 var data = e.target.result;
                 that.set('parentView.content', data);
-            }
+            };
             reader.readAsDataURL(input.files[0]);
         }
-    },
+    }
 });
+
+App.Sex = [
+    {
+        id : "",
+        name: "Select one..."
+    },
+    {
+        id : "M",
+        name: "Male"
+    },
+    {
+        id: "F",
+        name: "Female"
+    }
+];
