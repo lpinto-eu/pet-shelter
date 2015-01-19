8d133f8175f84f01f733902927db0a9fa8d5489f
lpinto-eu <mail@lpinto.eu>
2015-01-19 22:09:04 +0000

    Fix localhost url for serverside

--- src/main/webapp/js/app/adapters/application.js
+++ src/main/webapp/js/app/adapters/application.js
@@ -1,5 +1,5 @@
 App.ApplicationAdapter = DS.RESTAdapter.extend({
-    host: 'https://petshelter.info/app',
-//    host: 'https://localhost:8080',
+//    host: 'https://petshelter.info/app',
+    host: 'http://localhost:8080/petshelter',
     namespace: 'api'
 });
