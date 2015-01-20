d7964119f85a193c079ee75909e1d0a28b2d6093
lpinto-eu <mail@lpinto.eu>
2015-01-20 17:28:50 +0000

    Fix: silent error on image too big

--- src/main/webapp/js/app/controllers/animals/new.js
+++ src/main/webapp/js/app/controllers/animals/new.js
@@ -1,13 +1,17 @@
 App.AnimalsNewController = Ember.ObjectController.extend({
     actions: {
-        save: function() {
-            var self = this;
-            this.get('model').save()
-                .then(function() {
-                    self.transitionToRoute('animals.animal', self.get("id"));
-            });
+        save: function () {
+            if (this.get("model").get("picture").length > 65535) {
+                alert("Image is to big! max 50kb.");
+            } else {
+                var self = this;
+                this.get('model').save()
+                        .then(function () {
+                            self.transitionToRoute('animals.animal', self.get("id"));
+                        });
+            }
         },
-        cancel: function() {
+        cancel: function () {
             if (this.get('content.isDirty')) {
                 if (this.get('content.isNew') && confirm('Unsaved changes will be lost.')) {
                     this.get('content').deleteRecord();
