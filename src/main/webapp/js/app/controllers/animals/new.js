03d755521e65a6e0dfde576af0c931a77e291883
lpinto-eu <mail@lpinto.eu>
2015-01-25 23:41:20 +0000

    Fix broken save code on new animal

--- src/main/webapp/js/app/controllers/animals/new.js
+++ src/main/webapp/js/app/controllers/animals/new.js
@@ -1,7 +1,7 @@
 App.AnimalsNewController = Ember.ObjectController.extend({
     actions: {
         save: function () {
-            if (this.get("model").get("picture").length > 65535) {
+            if (this.get("model").get("picture") && this.get("model").get("picture").length > 65535) {
                 alert("Image is to big! max 50kb.");
             } else {
                 var self = this;
