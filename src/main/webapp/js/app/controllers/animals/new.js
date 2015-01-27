aa196786ccec1fa3bbaa3ddc180dadce98d2123a
lpinto-eu <mail@lpinto.eu>
2015-01-27 17:23:08 +0000

    New form: update image validation

--- src/main/webapp/js/app/controllers/animals/new.js
+++ src/main/webapp/js/app/controllers/animals/new.js
@@ -1,8 +1,10 @@
 App.AnimalsNewController = Ember.ObjectController.extend({
     actions: {
         save: function () {
-            if (this.get("model").get("picture") && this.get("model").get("picture").length > 65535) {
-                alert("Image is to big! max 50kb.");
+            if (this.get("model")
+                    && this.get("model").get("picture")
+                    && this.get("model").get("picture").length > 65535) {
+                alert("Image is too large! max 50kb.");
             } else {
                 var self = this;
                 this.get('model').save()
