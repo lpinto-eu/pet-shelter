c29948dac54f7cb30991456ab5a9c3bc885a6f3c
Vítor Martins <varmartins@varmartins.com>
2015-02-17 22:29:05 +0000

    Added Organizations
    Added controllers (new, organization and organizations), model
    (organization), routes(organization) and some changes to index and
    router

---
+++ src/main/webapp/js/app/serializers/organization.js
@@ -0,0 +1,11 @@
+App.OrganizationSerializer = DS.RESTSerializer.extend({
+    /* add root element on incoming json */
+    normalizePayload: function(payload) {
+        return {organization: payload};
+    },
+    /* remove root element from outgoing json */
+    serializeIntoHash: function(hash, type, record, options) {
+        Ember.merge(record.get("data"), this.serialize(record, options));
+        Ember.merge(hash, record.get("data"));
+    }
+});
