c868ea6f46c0f0fd9d6679d7679c3f137b07f256
Vítor Martins <varmartins@varmartins.com>
2015-02-17 22:29:05 +0000

    Added Organizations
    Added controllers (new, organization and organizations), model
    (organization), routes(organization) and some changes to index and
    router

---
+++ src/main/webapp/js/app/controllers/organizations/new.js
@@ -0,0 +1,21 @@
+App.AnimalsNewController = Ember.ObjectController.extend({
+    actions: {
+        save: function () {
+                var self = this;
+                this.get('model').save()
+                        .then(function () {
+                            self.transitionToRoute('organizations.organization', self.get("id"));
+                        });
+            }
+        },
+        cancel: function () {
+            if (this.get('content.isDirty')) {
+                if (this.get('content.isNew') && confirm('Unsaved changes will be lost.')) {
+                    this.get('content').deleteRecord();
+                    this.transitionToRoute('application');
+                }
+            }
+        }
+});
+
+
