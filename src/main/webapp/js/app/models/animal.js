bc91b02319bc2bf1b3e555e7b29649b1c1854976
Vítor Martins <varmartins@varmartins.com>
2015-03-05 21:07:25 +0000

    Location Java + Ember
    Everything related to the new property "Location"

--- src/main/webapp/js/app/models/animal.js
+++ src/main/webapp/js/app/models/animal.js
@@ -6,11 +6,24 @@ App.Animal = DS.Model.extend({
     drugs: DS.attr('string'),
     created: DS.attr('isodate'),
     sex: DS.attr('string'),
+    location: DS.attr(),
     sterilized: DS.attr('boolean'),
     picture: DS.attr('string'),
     observations: DS.attr('string'),
 
     recordDate: function () {
         return moment(this.get("created")).format('MMMM Do YYYY, h:mm:ss a');
-    }.property("content.created")
+    }.property("content.created"),
+    
+    isShelter: function () {
+        return this.get('location') === 1;
+    }.property('location'),
+
+    isAdopted: function () {
+        return this.get('location') === 2;
+    }.property('location'),
+    
+    isFostering: function () {
+        return this.get('location') === 3;
+    }.property('location'),
 });
