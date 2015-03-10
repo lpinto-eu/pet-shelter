d2d3ce7960a3a9e329930a625c77da1ecf107219
Vítor Martins <varmartins@varmartins.com>
2015-03-10 22:07:59 +0000

    Animal Proportion added
    Java and Ember animal Proportion added

---
+++ src/main/webapp/js/app/models/proportion.js
@@ -0,0 +1,23 @@
+/*
+ * 
+    @author Vítor Martins - varmartins@varmartins.com
+*/
+
+App.Proportion = [
+    {
+        name: "Select one..."
+    },
+    {
+        id : 1,
+        name: "Small"
+    },
+    {
+        id : 2,
+        name: "Medium"
+    },
+    {
+        id : 3,
+        name: "Large"
+    }
+];
+
