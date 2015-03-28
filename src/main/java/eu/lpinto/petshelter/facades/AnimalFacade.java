3e7f70af20a9e64b487de4537db54e6d0a223214
Pedro Mostardinha <yrs@192.168.0.16>
2015-03-28 14:13:51 +0000

    Data Model Change. User can now have multiple organizations, and organizations can have multiple users. Animal now has a valid JPA relation with Organization.

--- src/main/java/eu/lpinto/petshelter/facades/AnimalFacade.java
+++ src/main/java/eu/lpinto/petshelter/facades/AnimalFacade.java
@@ -1,16 +1,16 @@
 /**
- * AnimalFacade.java
- * Created on 02-Aug-2014, 21:43:14
+ * AnimalFacade.java Created on 02-Aug-2014, 21:43:14
  *
- * petshelter-webapp
- * petshelter-webapp
+ * petshelter-webapp petshelter-webapp
  *
  * Copyright (c) Pet Shelter - www.petshelter.info
  */
 package eu.lpinto.petshelter.facades;
 
 import eu.lpinto.petshelter.entities.Animal;
-import java.util.ArrayList;
+import eu.lpinto.petshelter.entities.Organization;
+import eu.lpinto.petshelter.entities.User;
+import java.util.Calendar;
 import java.util.List;
 //import java.util.stream.Collectors;
 import javax.ejb.Stateless;
@@ -28,23 +28,88 @@ public class AnimalFacade extends AbstractFacade<Animal> {
     @PersistenceContext(unitName = "eu.lpinto.petshelter_petshelter-webapp_war_1.0-SNAPSHOTPU")
     private EntityManager em;
 
+    /* Constructors */
     public AnimalFacade() {
         super(Animal.class);
     }
 
-    public List<Animal> findAllByOrg(final int orgID) {
-        List<Animal> result;
+    /* CRUD */
+    public List<Animal> findAll(int actionUserID, Integer orgID) {
 
-        List<Animal> animals = super.findAll();
-        result = new ArrayList<>(animals.size());
-
-        for (Animal animal : animals) {
-            if (animal.getOrganization() == orgID) {
-                result.add(animal);
-            }
+        User user = em.find(User.class, actionUserID);
+        em.refresh(user);
+        if (user == null) {
+            return null;
         }
 
-        return result;
+        Organization organization = getUserOrganization(user, orgID);
+        if (organization == null) {
+            return null;
+        }
+
+        /* Find All */
+        em.refresh(organization);
+        return organization.getAnimals();
+    }
+
+    public Animal retrieve(int actionUserID, Integer orgID, int animalID) {
+        User user = em.find(User.class, actionUserID);
+        em.refresh(user);
+        if (user == null) {
+            return null;
+        }
+
+        Organization organization = getUserOrganization(user, orgID);
+        if (organization == null) {
+            return null;
+        }
+
+        /* Find All */
+        em.refresh(organization);
+
+        return organization.getAnimal(animalID);
+    }
+
+    public Animal create(int actionUserID, Integer orgID, Animal animal) {
+        /* Check if User can do action */
+        User user = em.find(User.class, actionUserID);
+        if (user == null) {
+            return null;
+        }
+
+        Organization organization = getUserOrganization(user, orgID);
+        if (organization == null) {
+            return null;
+        }
+
+        animal.setCreated(Calendar.getInstance());
+        animal.setOrganization(organization);
+
+        super.create(animal);
+
+        return animal;
+    }
+
+    protected Organization getUserOrganization(User user, Integer orgID) {
+        Organization organization;
+        if (orgID == null) {
+            if (user.getOrganizations() != null && user.getOrganizations().size() == 1) {
+                /* User has only one Org */
+                organization = user.getOrganizations().get(0);
+
+            } else {
+                return null;
+            }
+
+        } else {
+            if (!user.hasOrganization(orgID)) {
+                return null;
+            }
+
+            organization = user.getOrganization(orgID);
+        }
+
+        return organization;
     }
 
     @Override
