d45dd93a02adb5ceeb2f902e1cec1e34cb99f449
lpinto-eu <mail@lpinto.eu>
2015-04-01 19:09:49 +0100

    Remove unnecessary fields of User - in ember model

--- src/main/webapp/js/app/models/user.js
+++ src/main/webapp/js/app/models/user.js
@@ -9,7 +9,6 @@ App.User = DS.Model.extend({
   created:               DS.attr(),
   updated:               DS.attr(),
   email:                 DS.attr('string'),
-  serialVersionUID:      DS.attr(),
   name:                  DS.attr('string'),
   password:              DS.attr('string'),
   apiKeys:               DS.hasMany('apiKey'),
