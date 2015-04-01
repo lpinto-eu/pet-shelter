b7cf5c75c0b3f3c3eb3dbef8b2a6972ecb63d8ae
lpinto-eu <mail@lpinto.eu>
2015-04-01 21:07:59 +0100

    Review of organizations CRUD - frontend

--- src/main/webapp/js/app/controllers/organizations/new.js
+++ src/main/webapp/js/app/controllers/organizations/new.js
@@ -1,21 +1,16 @@
 /*
- * 
+ *
     @author Vítor Martins - varmartins@varmartins.com
 */
 
 App.OrganizationsNewController = Ember.ObjectController.extend({
     actions: {
         save: function () {
-            if (this.get("model")
-                    && this.get("model").get("logo")
-                    && this.get("model").get("logo").length > 65535) {
-                alert("Logo is too large! max 50kb.");
-            } else {
-                var self = this;
-                this.get('model').save().then(function () {
+            var self = this;
+                this.get('model').save()
+                        .then(function () {
                             self.transitionToRoute('organizations.table');
                         });
-            }
         },
         cancel: function () {
             if (this.get('content.isDirty')) {
