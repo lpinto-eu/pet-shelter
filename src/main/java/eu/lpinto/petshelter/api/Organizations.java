775ad19529da0fada111f9615a4431439bf060bd
lpinto-eu <mail@lpinto.eu>
2015-04-03 01:03:33 +0100

    Organizations api rebuild

--- src/main/java/eu/lpinto/petshelter/api/Organizations.java
+++ src/main/java/eu/lpinto/petshelter/api/Organizations.java
@@ -1,6 +1,7 @@
 package eu.lpinto.petshelter.api;
 
 import eu.lpinto.petshelter.api.dts.OrganizationsDTS;
+import eu.lpinto.petshelter.api.filters.TrafficLogger;
 import eu.lpinto.petshelter.entities.Animal;
 import eu.lpinto.petshelter.entities.Organization;
 import eu.lpinto.petshelter.entities.User;
@@ -9,7 +10,6 @@ import eu.lpinto.petshelter.facades.OrganizationFacade;
 import eu.lpinto.petshelter.facades.UserFacade;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -22,8 +22,11 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -32,6 +35,8 @@ import javax.ws.rs.core.Response;
 @Singleton
 @Path("organizations")
 public class Organizations {
+
+    final Logger logger = LoggerFactory.getLogger(TrafficLogger.class);
 
     @EJB
     private OrganizationFacade orgFacade;
@@ -47,23 +52,23 @@ public class Organizations {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Organization> findAll(@HeaderParam("userID") final Integer actionUserID) {
+    public List<Organization> findAll(@HeaderParam("userID") final Integer userID) {
         try {
-            /* Find Organizations */
-            return orgFacade.findAll(actionUserID);
+            return userFacade.retrieve(userID).getOrganizations();
 
         } catch (RuntimeException ex) {
-            System.out.println(ex.getLocalizedMessage());
-            // TODO return http code!!
-            return new ArrayList<>(0);
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot retrieve organizations",
+                                              ex,
+                                              Response.Status.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GET
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
-    public Response export(@HeaderParam("userID") final Integer actionUserID) {
+    public Response export(@HeaderParam("userID") final Integer userID) {
         /* Find organizations and Build File */
-        String content = OrganizationsDTS.SINGLETON.transform(orgFacade.findAll(actionUserID));
+        String content = OrganizationsDTS.SINGLETON.transform(userFacade.retrieve(userID).getOrganizations());
         byte[] b = content.getBytes(Charset.forName("UTF-8"));
 
         return Response.ok(b).header("Content-Disposition", "attachment; filename=Organizations.csv").build();
@@ -72,41 +77,62 @@ public class Organizations {
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Organization retrieve(@HeaderParam("userID") final Integer actionUserID,
-                                 @PathParam("id") final int id) {
-        return orgFacade.retrieve(actionUserID, id);
+    public Organization retrieve(@HeaderParam("userID") final Integer userID, @PathParam("id") final int organizationID) {
+        Organization result;
+
+        try {
+            result = orgFacade.retrieve(organizationID);
+
+        } catch (Exception ex) {
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot retrieve organization", ex);
+        }
+
+        if (result == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        try {
+            result = userFacade.retrieve(userID).getOrganization(organizationID);
+
+        } catch (RuntimeException ex) {
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot retrieve organization", ex);
+        }
+        if (result == null) {
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
+
+        } else {
+            return result;
+        }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response create(@HeaderParam("userID") final Integer actionUserID, final Organization org) {
-        /* Check if User can do action */
-
+    public Response create(@HeaderParam("userID") final Integer userID, final Organization organization) {
         try {
             /* Create Organization */
-            orgFacade.create(org);
+            orgFacade.create(organization);
 
-            /* update User */
-            User user = userFacade.retrieve(actionUserID);
-            user.addOrg(org);
+            /* edit User */
+            User user = userFacade.retrieve(userID);
+            user.addOrg(organization);
             userFacade.edit(user);
 
-            return Response.ok(org).build();
+            return Response.ok(organization).build();
 
         } catch (RuntimeException ex) {
-            System.err.println(ex.getLocalizedMessage());
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Cannot create organization").build();
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot create organization", ex);
         }
     }
 
     @POST
     @Path("load")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void load(List<Organization> orgs) {
-        for (Organization animal : orgs) {
-            animal.setCreated(new GregorianCalendar());
-            animal.setUpdated(new GregorianCalendar());
+    public void load(List<Organization> organizations) {
+        for (Organization animal : organizations) {
             orgFacade.create(animal);
         }
     }
@@ -114,19 +140,61 @@ public class Organizations {
     @PUT
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void update(@PathParam("id") final int orgID,
+    public void update(@PathParam("id") final int organizationID,
                        @HeaderParam("userID") final Integer userID,
-                       Organization org) {
+                       final Organization organization) {
+        if (organization.getId() != null && organizationID != organization.getId()) {
+            throw new WebApplicationException("Cannot create organization", Response.Status.BAD_REQUEST);
+        }
 
-        org.setId(orgID);
-        orgFacade.update(userID, org);
+        organization.setId(organizationID);
+
+        Organization result;
+
+        try {
+            result = orgFacade.retrieve(organizationID);
+
+        } catch (Exception ex) {
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot retrieve organization", ex);
+        }
+
+        if (result == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+
+        try {
+            result = userFacade.retrieve(userID).getOrganization(organizationID);
+
+        } catch (RuntimeException ex) {
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot retrieve organization", ex);
+        }
+        if (result == null) {
+            throw new WebApplicationException(Response.Status.FORBIDDEN);
+
+        } else {
+
+            try {
+                orgFacade.edit(organization);
+
+            } catch (RuntimeException ex) {
+                logger.debug(ex.getLocalizedMessage(), ex);
+                throw new WebApplicationException("Cannot update organization", ex);
+            }
+        }
     }
 
     @DELETE
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void delete(@PathParam("id") final int orgID) {
-        orgFacade.remove(orgFacade.retrieve(orgID));
+    public void delete(@PathParam("id") final int organizationID) {
+        try {
+            orgFacade.remove(orgFacade.retrieve(organizationID));
+        } catch (RuntimeException ex) {
+            logger.debug(ex.getLocalizedMessage(), ex);
+            throw new WebApplicationException("Cannot delete organization", ex);
+        }
     }
 
     /*
@@ -136,11 +204,11 @@ public class Organizations {
     @Path("{id}/users/{targetUserID}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void associateUser(@HeaderParam("userID") final Integer actionUserID,
+    public void associateUser(@HeaderParam("userID") final Integer userID,
                               @PathParam("id") final int id,
                               @PathParam("targetUserID") final int targetUserID) {
         try {
-            orgFacade.associateUser(actionUserID, id, targetUserID);
+            orgFacade.associateUser(userID, id, targetUserID);
 
         } catch (RuntimeException ex) {
             throw ex;
@@ -151,11 +219,11 @@ public class Organizations {
     @Path("{id}/users/{targetUserID}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void dissociateUser(@HeaderParam("userID") final Integer actionUserID,
+    public void dissociateUser(@HeaderParam("userID") final Integer userID,
                                @PathParam("id") final int id,
                                @PathParam("targetUserID") final int targetUserID) {
         try {
-            orgFacade.dissociateUser(actionUserID, id, targetUserID);
+            orgFacade.dissociateUser(userID, id, targetUserID);
 
         } catch (RuntimeException ex) {
             throw ex;
@@ -168,11 +236,11 @@ public class Organizations {
     @GET
     @Path("{id}/animals")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Animal> findAllAnimals(@HeaderParam("userID") final Integer actionUserID,
+    public List<Animal> findAllAnimals(@HeaderParam("userID") final Integer userID,
                                        @PathParam("id") final int orgID) {
         try {
             /* Find Organizations */
-            return animalFacade.findAll(actionUserID, orgID);
+            return animalFacade.findAll(userID, orgID);
 
         } catch (RuntimeException ex) {
             System.out.println(ex.getLocalizedMessage());
@@ -184,12 +252,12 @@ public class Organizations {
     @GET
     @Path("{id}/animals/{animalID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Animal retrieveAnimal(@HeaderParam("userID") final Integer actionUserID,
+    public Animal retrieveAnimal(@HeaderParam("userID") final Integer userID,
                                  @PathParam("id") final int orgID,
                                  @PathParam("animalID") final int animalID) {
         try {
             /* Find Organizations */
-            return animalFacade.retrieve(actionUserID, orgID, animalID);
+            return animalFacade.retrieve(userID, orgID, animalID);
 
         } catch (RuntimeException ex) {
             System.out.println(ex.getLocalizedMessage());
@@ -201,12 +269,12 @@ public class Organizations {
     @POST
     @Path("{id}/animals")
     @Produces(MediaType.APPLICATION_JSON)
-    public Animal createAnimal(@HeaderParam("userID") final Integer actionUserID,
+    public Animal createAnimal(@HeaderParam("userID") final Integer userID,
                                @PathParam("id") final int orgID,
                                Animal animal) {
         try {
             /* Find Organizations */
-            return animalFacade.create(actionUserID, orgID, animal);
+            return animalFacade.create(userID, orgID, animal);
 
         } catch (RuntimeException ex) {
             System.out.println(ex.getLocalizedMessage());
