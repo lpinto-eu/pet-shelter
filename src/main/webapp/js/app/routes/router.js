65c60ed5b80c4a3cebf83fea08cf76f233607ebf
lpinto-eu <mail@lpinto.eu>
2015-04-12 16:37:04 +0100

    Router file indentation

--- src/main/webapp/js/app/routes/router.js
+++ src/main/webapp/js/app/routes/router.js
@@ -1,24 +1,26 @@
-App.Router.map(function() {
-    this.resource('error'),
-    this.resource('sessions', function() {
+App.Router.map(function () {
+    this.resource('error');
+
+    this.resource('sessions', function () {
         this.route('logout');
         this.route('login');
     });
-    this.resource('users', function() {
+
+    this.resource('users', function () {
         this.route('signup');
         this.route('user', {path: '/user/:user_id'}, function () {
             this.route('organizations');
         });
     });
 
-    this.resource('animals', function() {
-        this.route('list'),
-        this.route('table'),
+    this.resource('animals', function () {
+        this.route('list');
+        this.route('table');
         this.route('new');
         this.route("animal", {path: ":animal_id"});
     });
 
-    this.resource('organizations', function() {
+    this.resource('organizations', function () {
         this.route('new');
         this.route("organization", {path: ":organization_id"}, function () {
             this.route('users');
@@ -29,25 +31,22 @@ App.Router.map(function() {
 App.ApplicationRoute = Ember.Route.extend({
     actions: {
         // create a global logout action
-        logout: function() {
-          // get the sessions controller instance and reset it to then transition to the sessions route
-          this.controllerFor('sessions').reset();
-          Ember.$('#header-links').hide();
-          this.transitionTo('application');
+        logout: function () {
+            this.controllerFor('sessions').reset();
+            Ember.$('#header-links').hide();
+            this.transitionTo('application');
         },
-
-        error: function(reason, transition) {
+        error: function (reason, transition) {
             this.handleError(reason, transition);
         }
     },
-
     events: {
-        error: function(reason, transition) {
+        error: function (reason, transition) {
             this.handleError(reason, transition);
         }
     },
 
-    handleError: function(reason, transition) {
+    handleError: function (reason, transition) {
         if (reason.status === 401) {
             this.send('logout');
 
@@ -58,29 +57,28 @@ App.ApplicationRoute = Ember.Route.extend({
             console.log(reason.status + " - " + reason.statusText + " | " + reason.responseText);
             this.transitionTo("error", {queryParams: {status: reason.status}});
         }
-      }
+    }
 });
 
 App.SessionsRoute = Ember.Route.extend({
     // setup the SessionsController by resetting it to avoid data from a past authentication
-      setupController: function(controller, context) {
+    setupController: function (controller, context) {
         controller.reset();
-      },
-
-      beforeModel: function(transition) {
+    },
+    beforeModel: function (transition) {
         // before proceeding any further, verify if the token property is not empty
         // if it is, transition to the secret route
         if (!Ember.isEmpty(this.controllerFor('sessions').get('token'))) {
-          this.transitionTo('animals');
+            this.transitionTo('animals');
         }
-      }
+    }
 });
 
 // create a base object for any authentication protected route with the required verifications
 App.AuthenticatedRoute = Ember.Route.extend({
     // verify if the token property of the sessions controller is set before continuing with the request
     // if it is not, redirect to the login route (sessions)
-    beforeModel: function(transition) {
+    beforeModel: function (transition) {
         Ember.$('#header-links').show();
         if (Ember.isEmpty(this.controllerFor('sessions').get('token'))) {
             return this.redirectToLogin(transition);
@@ -88,23 +86,15 @@ App.AuthenticatedRoute = Ember.Route.extend({
     },
     // Redirect to the login page and store the current transition so we can
     // run it again after login
-    redirectToLogin: function(transition) {
+    redirectToLogin: function (transition) {
         this.controllerFor('sessions').set('attemptedTransition', transition);
         return this.transitionTo('sessions');
     }
 });
 
 App.UsersSignupRoute = Ember.Route.extend({
-  model: function() {
-    // define the model for the UsersSignupController as a new record from the User model
-    return this.store.createRecord('user');
-  }
-//  afterModel: function() {
-//    // instantiating a jQuery/Twitter Bootstrap datepicker component
-//    Ember.$(function() {
-//      Ember.$('.input-group.date').datepicker
-//        startView: 2,
-//        autoclose: true
-//    })
-//  }
+    model: function () {
+        // define the model for the UsersSignupController as a new record from the User model
+        return this.store.createRecord('user');
+    }
 });
