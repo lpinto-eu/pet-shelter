7c2ed2719f1b82824e16f3fd092bb1b677f1009e
lpinto-eu <mail@lpinto.eu>
2015-04-19 22:20:02 +0100

    Remove unused method from organization model

--- src/main/webapp/js/app/models/organization.js
+++ src/main/webapp/js/app/models/organization.js
@@ -1,5 +1,5 @@
 /*
- * 
+ *
     @author Vítor Martins - varmartins@varmartins.com
 */
 
@@ -7,9 +7,5 @@ App.Organization = DS.Model.extend({
     created:       DS.attr(),
     updated:       DS.attr(),
     name:          DS.attr('string'),
-    logo:          DS.attr(),
-
-    recordDate: function () {
-        return moment(this.get("created")).format('MMMM Do YYYY, h:mm:ss a');
-    }.property("content.created")
+    logo:          DS.attr()
 });
