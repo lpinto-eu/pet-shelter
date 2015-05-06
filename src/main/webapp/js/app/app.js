8f02f5be7bd5ac8e747807b32b77a4b3db9cf65e
dass87 <dasssoad@gmail.com>
2015-05-06 15:36:13 +0100

    Message Erros in Session

--- src/main/webapp/js/app/app.js
+++ src/main/webapp/js/app/app.js
@@ -1,3 +1,16 @@
 window.App = Ember.Application.create();
 
 App.ApplicationStore = DS.Store.extend();
+
+App.AlertController = Ember.Controller.extend({
+    alert: false,
+    observeAlert: function () {
+        if (this.alert != false) {
+            $('#flash').addClass('alert alert-' + this.alert[0] + ' alert-dismissable');
+            $('#flash span:last').text(this.alert[1]);
+            $('#flash').fadeIn();
+        } else {
+            $('#flash').hide();
+        }
+    }.observes('alert')
+});
