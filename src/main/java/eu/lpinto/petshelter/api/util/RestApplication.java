fa20d44be7991ffba0ea0d1bb18368d05e733a02
lpinto-eu <mail@lpinto.eu>
2015-05-07 09:26:00 +0100

    Rest logging and error object

--- src/main/java/eu/lpinto/petshelter/api/util/RestApplication.java
+++ src/main/java/eu/lpinto/petshelter/api/util/RestApplication.java
@@ -7,23 +7,30 @@
  *
  * Copyright (c) Trip Dashboard - www.tripdashboard.pt
  */
-
 package eu.lpinto.petshelter.api.util;
 
+import eu.lpinto.petshelter.api.filters.AccessTokenValidation;
+import java.util.logging.Logger;
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.filter.LoggingFilter;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
  * Root configuration of Jersey and the REST API.
- * 
- * @author Luis Pinto <code>- luis.pinto@tripdashboard.pt</code>
+ *
+ * @author Luis Pinto <code>- luis.pinto@petshelter.info</code>
  */
-@javax.ws.rs.ApplicationPath(RestConstants.REST_API_URI)
+@ApplicationPath(RestConstants.REST_API_URI)
 public class RestApplication extends ResourceConfig {
 
     public RestApplication() {
-        register( JacksonFeature.class );
+        register(JacksonFeature.class);
         register(JacksonConfigurator.class);
+
+        register(AccessTokenValidation.class);
+
         packages("eu.lpinto.petshelter.api.services");
-        packages("eu.lpinto.petshelter.api.filters");
+
+        registerInstances(new LoggingFilter(Logger.getLogger(RestApplication.class.getName()), true));
     }
 }
